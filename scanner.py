46fa55e64c5a2744bc095ef9bde369c1f34662b0
Napp1en <75671191+Napp1en@users.noreply.github.com>
2026-05-12 11:38:18 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -492,8 +492,8 @@ def format_history_summary(ws):
                     }],
                     "booleanRule": {
                         "condition": {
-                            "type": "NUMBER_LESS",
-                            "values": [{"userEnteredValue": "0.9"}],
+                            "type": "CUSTOM_FORMULA",
+                            "values": [{"userEnteredValue": "=G2<0.9"}],
                         },
                         "format": {
                             "backgroundColor": {"red": 0.65, "green": 0.85, "blue": 1.00},
